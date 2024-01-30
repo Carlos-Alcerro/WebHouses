10831c78d82e556f516f9445bdf5a01659ea4139
Carlos <carlosalcerrolainez2017@gmail.com>
2024-01-29 23:33:11 -0600

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from 'next'
-import { Inter, Quicksand } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter, Quicksand } from "next/font/google";
+import "./globals.css";
 
-const quickSand = Quicksand({ subsets: ['latin'] })
+const quickSand = Quicksand({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Real State - TarreDev',
-  description: 'Aplicación creada del video de Youtube de TarreDev',
-}
+  title: "Web Houses",
+  description: "Web creada para la venta y alquiler de casas",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="es">
       <body className={quickSand.className}>{children}</body>
     </html>
-  )
+  );
 }
